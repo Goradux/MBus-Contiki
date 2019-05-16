d5a74985bf839968cef08e8f1ecf792c68e17a99
Goradux <35964674+Goradux@users.noreply.github.com>
2019-05-16 18:50:19 +0200

    Update mbus_contiki.c
    Added more skeleton code

--- mbus_contiki.c
+++ mbus_contiki.c
@@ -5,6 +5,57 @@
 #define MBUS_FRAME_PURGE_M2S  1
 #define MBUS_FRAME_PURGE_NONE 0
 
+
+
+
+#define MBUS_FRAME_TYPE_ANY     0x00
+#define MBUS_FRAME_TYPE_ACK     0x01
+#define MBUS_FRAME_TYPE_SHORT   0x02
+#define MBUS_FRAME_TYPE_CONTROL 0x03
+#define MBUS_FRAME_TYPE_LONG    0x04
+
+#define MBUS_FRAME_ACK_START     0xE5
+#define MBUS_FRAME_SHORT_START   0x10
+#define MBUS_FRAME_CONTROL_START 0x68
+#define MBUS_FRAME_LONG_START    0x68
+#define MBUS_FRAME_STOP          0x16
+
+#define MBUS_RECV_RESULT_OK        0
+#define MBUS_RECV_RESULT_ERROR     -1
+#define MBUS_RECV_RESULT_INVALID   -2
+#define MBUS_RECV_RESULT_TIMEOUT   -3
+#define MBUS_RECV_RESULT_RESET     -4
+
+//
+// Control field
+//
+#define MBUS_CONTROL_FIELD_DIRECTION    0x07
+#define MBUS_CONTROL_FIELD_FCB          0x06
+#define MBUS_CONTROL_FIELD_ACD          0x06
+#define MBUS_CONTROL_FIELD_FCV          0x05
+#define MBUS_CONTROL_FIELD_DFC          0x05
+#define MBUS_CONTROL_FIELD_F3           0x04
+#define MBUS_CONTROL_FIELD_F2           0x03
+#define MBUS_CONTROL_FIELD_F1           0x02
+#define MBUS_CONTROL_FIELD_F0           0x01
+
+#define MBUS_CONTROL_MASK_SND_NKE       0x40
+#define MBUS_CONTROL_MASK_SND_UD        0x53
+#define MBUS_CONTROL_MASK_REQ_UD2       0x5B
+#define MBUS_CONTROL_MASK_REQ_UD1       0x5A
+#define MBUS_CONTROL_MASK_RSP_UD        0x08
+
+#define MBUS_CONTROL_MASK_FCB           0x20
+#define MBUS_CONTROL_MASK_FCV           0x10
+
+#define MBUS_CONTROL_MASK_ACD           0x20
+#define MBUS_CONTROL_MASK_DFC           0x10
+
+#define MBUS_CONTROL_MASK_DIR           0x40
+#define MBUS_CONTROL_MASK_DIR_M2S       0x40
+#define MBUS_CONTROL_MASK_DIR_S2M       0x00
+
+
 typedef struct _mbus_handle {
   int fd;
   int max_data_retry;
@@ -25,6 +76,7 @@ typedef struct _mbus_handle {
 
 
 
+
 typedef struct _mbus_serial_data
 {
     char *device;
@@ -33,6 +85,7 @@ typedef struct _mbus_serial_data
     struct termios t; //remove this
     // add some baud rate stuff here
 } mbus_serial_data;
+
 
 
 
@@ -79,6 +132,7 @@ mbus_handle * mbus_context_serial(const char *device) {
 
 
 
+
 int mbus_connect(mbus_handle *handle) {
   if (handle == NULL)
     {
@@ -93,6 +147,7 @@ int mbus_connect(mbus_handle *handle) {
 
 
 
+
 int mbus_disconnect(mbus_handle *handle) {
     if (handle == NULL)
     {
@@ -104,6 +159,7 @@ int mbus_disconnect(mbus_handle *handle) {
     //wrong ^^^
     // RS232_PORT_1 something
 }
+
 
 
 
@@ -141,6 +197,7 @@ int mbus_serial_set_baudrate(mbus_handle *handle, long baudrate) {
 
 
 
+
 void mbus_serial_data_free(mbus_handle *handle) {
     mbus_serial_data *serial_data;
 
@@ -157,7 +214,6 @@ void mbus_serial_data_free(mbus_handle *handle) {
         handle->auxdata = NULL;
     }
 }
-
 void mbus_context_free(mbus_handle * handle) {
     if (handle) {
         handle->free_auxdata(handle);
@@ -168,8 +224,218 @@ void mbus_context_free(mbus_handle * handle) {
 
 
 
+#define MBUS_FRAME_DATA_LENGTH 252
+
+typedef struct _mbus_frame {
+
+    unsigned char start1;
+    unsigned char length1;
+    unsigned char length2;
+    unsigned char start2;
+    unsigned char control;
+    unsigned char address;
+    unsigned char control_information;
+    // variable data field
+    unsigned char checksum;
+    unsigned char stop;
+
+    unsigned char   data[MBUS_FRAME_DATA_LENGTH];
+    size_t data_size;
+
+    int type;
+    time_t timestamp;
+
+    //mbus_frame_data frame_data;
+
+    void *next; // pointer to next mbus_frame for multi-telegram replies
+
+} mbus_frame;
+
+mbus_frame *mbus_frame_new(int frame_type);
+int         mbus_frame_free(mbus_frame *frame);
+
+mbus_frame *mbus_frame_new(int frame_type) {
+    mbus_frame *frame = NULL;
+
+    if ((frame = malloc(sizeof(mbus_frame))) != NULL)
+    {
+        memset((void *)frame, 0, sizeof(mbus_frame));
+        //no idea if memset() works on Contiki
+
+        frame->type = frame_type;
+        switch (frame->type)
+        {
+            case MBUS_FRAME_TYPE_ACK:
+                frame->start1 = MBUS_FRAME_ACK_START;
+                break;
+
+            case MBUS_FRAME_TYPE_SHORT:
+                frame->start1 = MBUS_FRAME_SHORT_START;
+                frame->stop   = MBUS_FRAME_STOP;
+                break;
+
+            case MBUS_FRAME_TYPE_CONTROL:
+                frame->start1 = MBUS_FRAME_CONTROL_START;
+                frame->start2 = MBUS_FRAME_CONTROL_START;
+                frame->length1 = 3;
+                frame->length2 = 3;
+                frame->stop   = MBUS_FRAME_STOP;
+                break;
+
+            case MBUS_FRAME_TYPE_LONG:
+                frame->start1 = MBUS_FRAME_LONG_START;
+                frame->start2 = MBUS_FRAME_LONG_START;
+                frame->stop   = MBUS_FRAME_STOP;
+                break;
+        }
+    }
+    return frame;
+}
+int mbus_frame_free(mbus_frame *frame) {
+    if (frame)
+    {
+        if (frame->next != NULL)
+            mbus_frame_free(frame->next);
+        free(frame);
+        return 0;
+    }
+    return -1;
+}
 
 
+
+//---------------------------------------------------------
+// Checks if an integer is a valid primary address.
+//---------------------------------------------------------
+int mbus_is_primary_address(int value) {
+    return ((value >= 0x00) && (value <= 0xFF));
+}
+//------------------------------------------------------------------------------
+// send a data request packet to from master to slave and optional purge response
+//------------------------------------------------------------------------------
+int mbus_send_ping_frame(mbus_handle *handle, int address, char purge_response) {
+    int retval = 0;
+    mbus_frame *frame;
+
+    if (mbus_is_primary_address(address) == 0)
+    {
+        printf("Invalid address: %d\n", address);
+        return 1;
+    }
+
+    frame = mbus_frame_new(MBUS_FRAME_TYPE_SHORT);
+
+    if (frame == NULL)
+    {
+        printf("Failed to allocate M-Bus frame.\n");
+        return -1;
+    }
+
+    frame->control  = MBUS_CONTROL_MASK_SND_NKE | MBUS_CONTROL_MASK_DIR_M2S;
+    frame->address  = address;
+
+    if (mbus_send_frame(handle, frame) == -1)
+    //^^^ check this
+    {
+        printf("Failed to send M-Bus frame.\n");
+        mbus_frame_free(frame);
+        return -1;
+    }
+
+    if (purge_response)
+    {
+        //mbus_purge_frames(handle);
+        //purge frames here
+    }
+
+    mbus_frame_free(frame);
+    return retval;
+}
+
+int mbus_recv_frame(mbus_handle * handle, mbus_frame *frame) {
+    int result = 0;
+
+    if (handle == NULL)
+    {
+        printf("Invalid M-Bus handle for receive.\n");
+        return MBUS_RECV_RESULT_ERROR;
+    }
+
+    if (frame == NULL)
+    {
+        printf("Invalid frame.\n");
+        return MBUS_RECV_RESULT_ERROR;
+    }
+
+    result = handle->recv(handle, frame);
+
+    switch (mbus_frame_direction(frame))
+    {
+        case MBUS_CONTROL_MASK_DIR_M2S:
+            if (handle->purge_first_frame == MBUS_FRAME_PURGE_M2S)
+                result = handle->recv(handle, frame);  // purge echo and retry
+            break;
+        case MBUS_CONTROL_MASK_DIR_S2M:
+            if (handle->purge_first_frame == MBUS_FRAME_PURGE_S2M)
+                result = handle->recv(handle, frame);  // purge echo and retry
+            break;
+    }
+
+    if (frame != NULL)
+    {
+        /* set timestamp to receive time */
+        time(&(frame->timestamp));
+    }
+
+    return result;
+}
+
+int ping_address(mbus_handle *handle, mbus_frame *reply, int address) {
+    int i, ret = MBUS_RECV_RESULT_ERROR;
+
+    memset((void *)reply, 0, sizeof(mbus_frame));
+    //^^^ no idea if Contiki supports memset()
+
+    for (i = 0; i <= handle->max_search_retry; i++)
+    {
+        if (mbus_send_ping_frame(handle, address, 0) == -1)
+        {
+            fprintf(stderr,"Scan failed. Could not send ping frame: %s\n", mbus_error_str());
+            printf("Scan failed. Could not send ping frame.\n");
+            return MBUS_RECV_RESULT_ERROR;
+        }
+
+        ret = mbus_recv_frame(handle, reply);
+
+        if (ret != MBUS_RECV_RESULT_TIMEOUT)
+        {
+            return ret;
+        }
+    }
+
+    return ret;
+}
+
+
+
+
+
+/*
+
+finish up mbus_recv_frame() and mbus_send_ping_frame()
+
+*/
+
+
+
+
+
+
+
+
+
+
+//main function
 int mbus_contiki_scan(int baudrate) {
   mbus_handle *handle;
   char *device;
@@ -177,6 +443,7 @@ int mbus_contiki_scan(int baudrate) {
   //device = RS232_PORT_1;
   int address, retries = 0;
   int baudrate = 9600;
+  int ret;
 
   if ((handle = mbus_context_serial(device)) == NULL)
     {
@@ -196,17 +463,33 @@ int mbus_contiki_scan(int baudrate) {
       return -1;
     }
 
-
-
     /* Main scanning part*/
     for (address = 0; address <= 250; address++) {
       mbus_frame reply;
+
+      ret = ping_address(handle, &reply, address);
+
+      if (ret == MBUS_RECV_RESULT_TIMEOUT)
+            continue;
+
+      if (ret == MBUS_RECV_RESULT_INVALID)
+        {
+            //purge frames here
+            printf("Collision at address %d\n", address);
+            continue;
+        }
+
+      if (mbus_frame_type(&reply) == MBUS_FRAME_TYPE_ACK)
+        {
+            /* check for more data (collision) */
+            if (0 /*purge frames here*/)
+            {
+                printf("Collision at address %d\n", address);
+                continue;
+            }
+            printf("Found a M-Bus device at address %d\n", address);
+        }
     }
-
-
-
-
-
 
     mbus_disconnect(handle);
     mbus_context_free(handle);
